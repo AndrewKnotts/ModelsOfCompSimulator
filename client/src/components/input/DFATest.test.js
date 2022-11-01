b8841f90a48edeb15a5ffd3fc99e5aa0d925a686
alyssajjc <junwei@live.unc.edu>
2022-11-01 15:00:31 -0400

    wrote unit tests for DFAModel, bug fixes for DFA/NFAModels

--- client/src/components/input/DFATest.test.js
+++ client/src/components/input/DFATest.test.js
@@ -1,100 +1,337 @@
-import { DFAModel, Transition } from './DFAModel.js';
+import { DFAModel, Transition, State, parseAlphabet, parseStates, parseTransition } from './DFAModel.js';
 
 it('test works', () => {
   expect(1+2).toEqual(3);
 });
 
 it('rejects model with empty alphabet', () => {
-    // write test
-    expect(true).toBe(true);
-});
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0, s1, s2";
+    let alphabet = "";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let DFA_test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("Empty alphabet");
+}); 
 
 it('rejects model with duplicates in alphabet', () => {
-    // write test
-    expect(true).toBe(true);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0, s1, s2";
+    let alphabet = "0,1,0";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let DFA_test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("Duplicate in alphabet: 0");
 });
 
 it('rejects model with empty states', () => {
-    // write test
-    expect(true).toBe(true);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("Empty States");
 });
 
 it('rejects model with duplicates in states', () => {
-    // write test
-    expect(true).toBe(true);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0,s1,s2,s0";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("Conflicting state name: s0");
 });
 
 it('rejects model with alphabet symbol as a state', () => {
-    // write test
-    expect(true).toBe(true);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0,s1,1,s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("Conflicting state name: 1");
 });
 
 it('rejects model with empty initial', () => {
-    // write test
-    expect(true).toBe(true);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "";
+    let accepting = "s0";
+    let all_states = "s0,s1,s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("Invalid initial state");
 });
 
 it('rejects model with invalid initial', () => {
-    // write test
-    expect(true).toBe(true);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "0";
+    let accepting = "s0";
+    let all_states = "s0,s1,s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("Invalid initial state");
 });
 
 it('rejects model with empty accepting', () => {
-    // write test
-    expect(true).toBe(true);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "";
+    let all_states = "s0,s1,s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("Empty accepting states");
 });
 
 it('rejects model with invalid accepting', () => {
-    // write test
-    expect(true).toBe(true);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0,s3";
+    let all_states = "s0,s1,s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("Accepting state s3 does not exist");
 });
 
 it('rejects model with empty transitions', () => {
-    // write test
-    expect(true).toBe(true);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0,s1,s2";
+    let alphabet = "0,1";
+    let transitions = "";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("Empty transitions");
 });
 
 it('rejects model with invalid symbol of transition', () => {
-    // write test
-    expect(true).toBe(true);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0,s1,s2";
+    let alphabet = "0,1";
+    let transitions = "(a, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("Invalid symbol in transition: (a, s0, s0)");
 });
 
 it('rejects model with invalid source of transition', () => {
-    // write test
-    expect(true).toBe(true);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0,s1,s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("Invalid source in transition: (0, 1, s2)");
 });
 
 it('rejects model with invalid destination of transition', () => {
-    // write test
-    expect(true).toBe(true);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0,s1,s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s3);(1,s2,s2)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("Invalid destination in transition: (0, s2, s3)");
 });
 
 it('rejects model with duplicate transition', () => {
-    // write test
-    expect(true).toBe(true);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0,s1,s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2);(0,s0,s0)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("State s0 has multiple transitions for symbol 0");
 });
 
 it('rejects model with two dests for one src/symbol', () => {
-    // write test
-    expect(true).toBe(true);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0,s1,s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2);(1,s2,s0)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("State s2 has multiple transitions for symbol 1");
 });
 
 it('rejects model with no transitions for a state', () => {
-    // write test
-    expect(true).toBe(true);
-});
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0,s1,s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("State s2 has no transitions");
+}); 
 
 it('rejects model with missing symbols for a state', () => {
-    // write test
-    expect(true).toBe(true);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0,s1,s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    expect(window.alert).toBeCalledWith("State s2 has no transition for symbol 1");
 });
 
-it('accepts sample string', () => {
-    // write test
-    expect(true).toBe(true);
+it('accepts sample string 1 for test model 1', () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0, s1, s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2);";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    test.checkInputString("10111110100");
+    expect(test.acceptance_result).toBe(true);
 });
 
-it('rejects sample string', () => {
-    // write test
-    expect(true).toBe(true);
+it('accepts sample string 2 for test model 1', () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0, s1, s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    test.checkInputString("101111101");
+    expect(test.acceptance_result).toBe(true);
+});
+
+it('accepts sample string 3 for test model 1', () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0, s1, s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    test.checkInputString("1001");
+    expect(test.acceptance_result).toBe(true);
+});
+
+it('rejects sample string 1 for test model 1', () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0, s1, s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    test.checkInputString("1011111011");
+    expect(test.acceptance_result).toBe(false);
+});
+
+it('rejects sample string 2 for test model 1', () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0, s1, s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    test.checkInputString("10");
+    expect(test.acceptance_result).toBe(false);
+});
+
+it('rejects sample string 3 for test model 1', () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "s0";
+    let accepting = "s0";
+    let all_states = "s0, s1, s2";
+    let alphabet = "0,1";
+    let transitions = "(0, s0, s0); (1,s0,s1);(0,s1,s2);(1,s1,s0);(0,s2,s1);(1,s2,s2)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    test.checkInputString("1010101");
+    expect(test.acceptance_result).toBe(false);
+});
+
+it('accepts sample string 1 for test model 2', () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "q0";
+    let accepting = "q3,q4";
+    let all_states = "q0,q1,q2,q3,q4";
+    let alphabet = "0,1";
+    let transitions = "(0,q0,q1);(1,q0,q2);(0,q1,q3);(1,q1,q2);(0,q2,q1);(1,q2,q4);(0,q3,q3);(1,q3,q2);(0,q4,q1);(1,q4,q4)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    test.checkInputString("0000000");
+    expect(test.acceptance_result).toBe(true);
+});
+
+it('accepts sample string 2 for test model 2', () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "q0";
+    let accepting = "q3,q4";
+    let all_states = "q0,q1,q2,q3,q4";
+    let alphabet = "0,1";
+    let transitions = "(0,q0,q1);(1,q0,q2);(0,q1,q3);(1,q1,q2);(0,q2,q1);(1,q2,q4);(0,q3,q3);(1,q3,q2);(0,q4,q1);(1,q4,q4)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    test.checkInputString("111111");
+    expect(test.acceptance_result).toBe(true);
+});
+
+it('accepts sample string 3 for test model 2', () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "q0";
+    let accepting = "q3,q4";
+    let all_states = "q0,q1,q2,q3,q4";
+    let alphabet = "0,1";
+    let transitions = "(0,q0,q1);(1,q0,q2);(0,q1,q3);(1,q1,q2);(0,q2,q1);(1,q2,q4);(0,q3,q3);(1,q3,q2);(0,q4,q1);(1,q4,q4)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    test.checkInputString("0000011");
+    expect(test.acceptance_result).toBe(true);
+});
+
+
+it('rejects sample string 1 for test model 2', () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "q0";
+    let accepting = "q3,q4";
+    let all_states = "q0,q1,q2,q3,q4";
+    let alphabet = "0,1";
+    let transitions = "(0,q0,q1);(1,q0,q2);(0,q1,q3);(1,q1,q2);(0,q2,q1);(1,q2,q4);(0,q3,q3);(1,q3,q2);(0,q4,q1);(1,q4,q4)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    test.checkInputString("000001");
+    expect(test.acceptance_result).toBe(false);
+});
+
+it('rejects sample string 2 for test model 2', () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "q0";
+    let accepting = "q3,q4";
+    let all_states = "q0,q1,q2,q3,q4";
+    let alphabet = "0,1";
+    let transitions = "(0,q0,q1);(1,q0,q2);(0,q1,q3);(1,q1,q2);(0,q2,q1);(1,q2,q4);(0,q3,q3);(1,q3,q2);(0,q4,q1);(1,q4,q4)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    test.checkInputString("010101010");
+    expect(test.acceptance_result).toBe(false);
+});
+
+it('rejects sample string 3 for test model 2', () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    let initial = "q0";
+    let accepting = "q3,q4";
+    let all_states = "q0,q1,q2,q3,q4";
+    let alphabet = "0,1";
+    let transitions = "(0,q0,q1);(1,q0,q2);(0,q1,q3);(1,q1,q2);(0,q2,q1);(1,q2,q4);(0,q3,q3);(1,q3,q2);(0,q4,q1);(1,q4,q4)";
+    let test = new DFAModel(initial, accepting, all_states, alphabet, transitions);
+    test.checkInputString("0");
+    expect(test.acceptance_result).toBe(false);
 });
--- client/src/components/input/NFAModel.js
+++ client/src/components/input/NFAModel.js
@@ -65,6 +65,7 @@ export class NFAModel {
         this.states = null;
         this.error = null;
         this.setPath = [];
+        this.input = null;
         //this.ts = new Map();
 
         console.log("initial: ", initial);
@@ -74,18 +75,24 @@ export class NFAModel {
         console.log("transitions: ", transitions);
 
         // check components and alert if error
-        if (!this.checkAlphabet()) window.alert(this.error);
-        if (!this.checkStates()) window.alert(this.error);
-        if (!this.checkInitial(initial)) window.alert(this.error);
-        if (!this.checkAccepting(accepting)) window.alert(this.error);
-        if (!this.checkTransitions()) window.alert(this.error);
-
-        // make Connected for all and check
-        this.makeConnected(this.initial);
-        for (let i in this.all) {
-            let s = this.all[i];
-            if (!s.connected) {
-                console.log("State " + s.name + " is not reachable.");
+        if (!this.checkAlphabet()) {
+            window.alert(this.error);
+        } else if (!this.checkStates()) {
+            window.alert(this.error);
+        } else if (!this.checkInitial(initial)) {
+            window.alert(this.error);
+        } else if (!this.checkAccepting(accepting)) {
+            window.alert(this.error);
+        } else if (!this.checkTransitions()) {
+            window.alert(this.error);
+        } else {
+            // make Connected for all and check
+            this.makeConnected(this.initial);
+            for (let i in this.all) {
+                let s = this.all[i];
+                if (!s.connected) {
+                    console.log("State " + s.name + " is not reachable.");
+                }
             }
         }
     }
@@ -114,6 +121,7 @@ export class NFAModel {
         // create Set to represent next states and initialize current Set to initial state
         let next = new Set();
         this.current = new Set().add(this.initial);
+        this.input = input;
 
         // add all epsilon transitons from initial state
         let eps_trans = this.initial.getEpsilonTrans();
@@ -162,87 +170,9 @@ export class NFAModel {
         return this.acceptString();
     }
 
-    /*
-    // given a path, checks whether end state is accepting and adds success/fail marker if so
-    acceptString(current, path) {
-        // Check if the current state is an accepting state
-        let ret_path = [...path];
-        for (let i in this.accepting) {
-            let s = this.accepting[i];
-            if (s == current) {
-                // create acceptance visualization and add it to end of path
-                let accept_state = new NFAState("🙂");
-                let acceptance = new Pair("✔️", accept_state);
-                ret_path.push(acceptance);
-                return ret_path;
-            }
-        }
-        // if not accepting, create fail visualization and add it to end of path
-        let fail_state = new NFAState("🙁");
-        let failure = new Pair("❌", fail_state);
-        ret_path.push(failure);
-        return ret_path;
-    }
-    
-    // call checkPath
-    checkInputString(input) {
-        console.log(input);
-        // Pass initial state and input as current and create new path starting from initial
-        let init_path = [new Pair("", this.initial)];
-        return this.checkPath(input + "\\", this.initial, init_path);
-    }
-
-    // recursively check path for acceptance
-    checkPath(input, current, path) {
-        console.log("CheckPath: ");
-        console.log(path);
-        // if string is down to the final "\", return the path
-        if (input === "\\") {
-            // if accepting string, return the path immediately
-            console.log(path);
-            let acceptance = this.acceptString(current, path);
-            console.log("Path 2: ")
-            console.log(path);
-            if (acceptance[acceptance.length - 1].right.name == "🙂") {
-                return acceptance;
-            }
-
-            // if not accepting, check for any epsilon transitions
-            for (let trans of current.getTrans()) {
-                if (trans.left == "eps") {
-                    path.push(trans);
-                    return this.checkPath(input, trans.right, path);
-                }
-            }
-
-            return acceptance;
-        } 
-        
-        // Take first char of string, check transitions for match, update to new state and repeat when found
-        let str = input.substring(0, 1);
-        // input string fails if a character is not in alphabet
-        if (!this.syms.has(str)) {
-            window.alert("Character " + str + " is not in alphabet");
-            return null;
-        }
-        
-        // Check each transition in the list, if symbol matches or epsilon, recur on the next step down the path
-        for (let trans of current.getTrans()) {
-            console.log("Trans: " + trans);
-            if (str == trans.left) {
-                path.push(trans);
-                return this.checkPath(input.substring(1), trans.right, path);
-            }
-            if (trans.left == "eps") {
-                path.push(trans);
-                return this.checkPath(input, trans.right, path);
-            }
-        }
-    } */
-
     // Checks alphabet for repeated symbols or being empty
     checkAlphabet() {
-        if (this.alphabet.size === 0) {
+        if (this.alphabet.length === 0) {
             this.error = "Empty alphabet";
             return false;
         }
@@ -263,8 +193,9 @@ export class NFAModel {
     }
 
     // Checks states for repeats or conflicts with alphabet
+    // Creates a Map of state names to States (this.states)
     checkStates() {
-        if (this.all.size === 0) {
+        if (this.all.length === 0) {
             this.error = "Empty States";
             return false;
         }
@@ -283,7 +214,7 @@ export class NFAModel {
         return true;
     }
 
-    // Checks that initial state is a valid state and assigns it
+    // Checks that initial state is a valid state and assigns this.initial to the State
     checkInitial(initial) {
         if (this.states.has(initial)) {
             this.initial = this.states.get(initial);
@@ -295,9 +226,10 @@ export class NFAModel {
     }
 
     // Checks that accepting states are present and valid
+    // Creates an array of the appropriate accepting States in this.accepting
     checkAccepting(accepting) {
         let acc_array = parseAlphabet(accepting);
-        if (acc_array.size === 0) {
+        if (acc_array.length === 0) {
             this.error = "Empty accepting states";
             return false;
         }
@@ -314,7 +246,7 @@ export class NFAModel {
         return true;
     }
 
-    // Checks that transitions are valid symbols/states/states
+    // Checks that transitions are valid symbols/states/states, and are not duplicates of prior transitions
     // Sets up connections to check all states are connected
     checkTransitions() {
         if (this.transitions.size === 0) {
@@ -325,7 +257,6 @@ export class NFAModel {
         for (let i in this.transitions) {
             let t = this.transitions[i];
 
-            // confirm that the symbol and source/dest states exist 
             if (!this.syms.has(t.symbol)) {
                 this.error = "Invalid symbol in transition: (" + t.symbol + ", " + t.source + ", " + t.dest + ")";
                 return false;
@@ -346,14 +277,13 @@ export class NFAModel {
             for (let j in src_state.getAllTrans()) {
                 let ts = src_state.getAllTrans()[j];
                 if ((ts.left == t.symbol) && (ts.right == t.dest)) {
-                    // duplicate transition
                     this.error = "Transition (" + t.symbol + ", " + t.source.name + ", " + t.dest.name + ") already exists";
                     return false;
                 }
             }
             src_state.addTrans(t.symbol, t.dest);
 
-            // add for connection check
+            // Add the dest State to source.conn for connection check
             if (!t.source.conn.includes(t.dest)) {
                 t.source.conn.push(t.dest);
             }
@@ -375,11 +305,13 @@ export class NFAModel {
 
 // parse states string input into NFAState array
 export function parseNFAStates(input) {
+    input = input.replaceAll(" ", "");
+
     let input_array = input.split(',');
     let states_array = [];
     for (let i in input_array) {
         let str_state = input_array[i];
-        states_array[i] = new NFAState(str_state.replace(" ", ""));
+        states_array[i] = new NFAState(str_state);
     }
     return states_array;
 }
