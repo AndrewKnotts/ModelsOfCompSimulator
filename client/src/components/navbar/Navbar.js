812ba54580b2230b2d1d6ee63050110b10cf9aa7
Andrew Knotts <knottsan2010@gmail.com>
2022-11-10 18:25:13 -0500

    updated css

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,13 +1,15 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
 import './Navbar.css'
+import logo1 from '../../images/logo1.png';
 
 export default function Navbar() {
     return (
         <nav className="nav">
-            <Link to="/" className="site-title">
+            <img src={logo1} className="img"/>
+            <h1 to="/" className="site-title">
                 Models Of Computation Simulator
-            </Link>
-            <ul>
+            </h1>
+            <ul className="navcat">
                 <CustomLink to="/">DFA</CustomLink>
                 <CustomLink to="/nfa">NFA</CustomLink>
                 <CustomLink to="/pda">PDA</CustomLink>
