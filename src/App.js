ea8fca304bb3fccb1e3f01443fc5500d08f1f169
justinadams <jmadams3@live.unc.edu>
2022-11-18 00:06:29 -0500

    changes to routing - hope it fixes github pages bug

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
-import { Routes, Route, BrowserRouter, Router } from "react-router-dom";
+import { Routes, Route, BrowserRouter, HashRouter } from "react-router-dom";
 import Turing from './pages/Turing';
 import DFA from './pages/DFA';
 import NFA from './pages/NFA';
 import PDA from './pages/PDA';
 import InputArea from './components/input/InputArea';
-//import Graph from './components/graph/Graph';
 import { Component } from 'react';
-import { page } from './components/navbar/Navbar';
-import { DFAModel, Transition } from './components/input/DFAModel.js';
-import { NFAModel } from './components/input/NFAModel.js';
-import State from './components/state/State';
-import Arrow from './components/arrow/arrow';
 import Navbar from './components/navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -21,14 +15,16 @@ export default class App extends Component {
   render() {
     return (
       <>
-        <Navbar />
         <div className="container">
-          <Routes>
-            <Route path="/" element={<DFA />} />
-            <Route path="/nfa" element={<NFA />} />
-            <Route path="/pda" element={<PDA />} />
-            <Route path="/turing" element={<Turing />} />
-          </Routes>
+          <HashRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<DFA />} />
+              <Route path="/nfa" element={<NFA />} />
+              <Route path="/pda" element={<PDA />} />
+              <Route path="/turing" element={<Turing />} />
+            </Routes>
+          </HashRouter>
         </div>
       </>
     );
